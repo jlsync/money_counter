7e4b396e8f75d08fef1dca6a3d9e6e5797204511
Jason Lee <jlsync@gmail.com>
2010-09-18 19:15:37 +0100

    rails3 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyCounter::Application.config.secret_token = '5ab7dd92185fd047ccbcb7c61037186d9f0cb36d0ccbafdf2049151336f62af3fb75884dff795cd90f3ae23b6340023c73714fa7e4ce89154091edb83327d0b9'
