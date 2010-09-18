d268bd6bc2de899d154bbcb8815c9fbec64d7c7c
nas <nas35_in@yahoo.com>
2010-09-18 22:15:39 +0100

    Remove all unnecessary code from controller except new and create

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,77 +1,20 @@
 class ImagesController < ApplicationController
-  # GET /images
-  # GET /images.xml
-  def index
-    #@images = Image.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
-
-  # GET /images/1
-  # GET /images/1.xml
-  def show
-    #@image = Image.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
-  end
-
-  # GET /images/new
-  # GET /images/new.xml
   def new
-
     respond_to do |format|
       format.html # new.html.erb
     end
   end
 
-  # GET /images/1/edit
-  def edit
-    #@image = Image.find(params[:id])
-  end
-
-  # POST /images
   def create
-
-    @filename = params[:upload][:image_file].original_filename
-    uploaded = params[:upload][:image_file].read
-
-    f = File.open("/tmp/" + @filename, "w")
-    f.write uploaded
-    f.close
+    image = Image.new(:file => params[:upload][:image_file])
+    image.save!
 
     respond_to do |format|
-      format.html { render :text => "the image filename was #{@filename} and it's size was #{uploaded.size.to_s} and the amount of money was #{"XXX.XX"}" }
+      format.html { render :text => "the image filename was #{image.file_name} and it's size was #{image.data.size} and the amount of money was #{"XXX.XX"}" }
+      format.xml { render :xml => image.new.to_xml }
     end
 
   end
 
-  # PUT /images/1
-  # PUT /images/1.xml
-  def update
-    @image = Image.find(params[:id])
-
-    respond_to do |format|
-      if @image.update_attributes(params[:image])
-        format.html { redirect_to(@image, :notice => 'Image was successfully updated.') }
-      else
-        format.html { render :action => "edit" }
-      end
-    end
-  end
-
-  # DELETE /images/1
-  # DELETE /images/1.xml
-  def destroy
-   # @image = Image.find(params[:id])
-   # @image.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(images_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
